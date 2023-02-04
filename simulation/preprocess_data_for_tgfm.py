20fa99e380eb71e754ba5fe81001088308f765e0
BennyStrobes <bstrobe1@jhu.edu>
2023-02-04 11:45:03 -0500

    update

--- simulation/preprocess_data_for_tgfm.py
+++ simulation/preprocess_data_for_tgfm.py
@@ -115,21 +115,21 @@ def create_anno_matrix_for_set_of_rsids(rsid_to_genomic_annotation, window_rsids
 	return np.asarray(anno_mat_arr)
 
 
-def compute_log_expected_probability(full_anno_mat, sldsc_tau_mean):
+def compute_log_expected_probability(full_anno_mat, sldsc_tau_mean, threshold):
 	expected_per_ele_h2 = np.dot(full_anno_mat, sldsc_tau_mean)
-	expected_per_ele_h2[expected_per_ele_h2 <= 1e-30] = 1e-30
+	expected_per_ele_h2[expected_per_ele_h2 <= threshold] = threshold
 
 	prob = expected_per_ele_h2/np.sum(expected_per_ele_h2)
 
 	return np.log(prob)
 
-def compute_expected_log_probability(full_anno_mat, sldsc_tau_mean, sldsc_tau_cov, n_samples=10000):
+def compute_expected_log_probability(full_anno_mat, sldsc_tau_mean, sldsc_tau_cov, threshold, n_samples=10000):
 	# Sample a whole bunch of taus
 	sampled_taus = np.random.multivariate_normal(mean=sldsc_tau_mean, cov=sldsc_tau_cov, size=n_samples)
 
 	sampled_expected_per_ele_h2 = np.dot(full_anno_mat, np.transpose(sampled_taus))
 
-	sampled_expected_per_ele_h2[sampled_expected_per_ele_h2 <= 1e-30] = 1e-30
+	sampled_expected_per_ele_h2[sampled_expected_per_ele_h2 <= threshold] = threshold
 
 	prob = np.copy(sampled_expected_per_ele_h2)
 	for sample_iter in range(n_samples):
@@ -141,7 +141,7 @@ def compute_expected_log_probability(full_anno_mat, sldsc_tau_mean, sldsc_tau_co
 
 
 
-def compute_various_versions_of_log_prior_probabilities(window_rsids, window_snp_anno_mat, gene_tissue_pairs, sldsc_tau_mean, sldsc_tau_cov, sparse_sldsc_tau):
+def compute_various_versions_of_log_prior_probabilities(window_rsids, window_snp_anno_mat, gene_tissue_pairs, sldsc_tau_mean, sldsc_tau_cov, sparse_sldsc_tau, threshold=1e-30):
 	# Merge together window_snp_anno_mat with tissue_anno_mat
 	n_snps = len(window_rsids)
 	n_genes = len(gene_tissue_pairs)
@@ -160,12 +160,82 @@ def compute_various_versions_of_log_prior_probabilities(window_rsids, window_snp
 		pdb.set_trace()
 
 
-	point_estimate_ln_pi = compute_log_expected_probability(full_anno_mat, sldsc_tau_mean)
-	sparse_estimate_ln_pi = compute_log_expected_probability(full_anno_mat, sparse_sldsc_tau)
-	distribution_estimate_ln_pi = compute_expected_log_probability(full_anno_mat, sldsc_tau_mean, sldsc_tau_cov)
+	point_estimate_ln_pi = compute_log_expected_probability(full_anno_mat, sldsc_tau_mean, threshold)
+	sparse_estimate_ln_pi = compute_log_expected_probability(full_anno_mat, sparse_sldsc_tau, threshold)
+	distribution_estimate_ln_pi = compute_expected_log_probability(full_anno_mat, sldsc_tau_mean, sldsc_tau_cov, threshold)
 
 	return point_estimate_ln_pi, sparse_estimate_ln_pi, distribution_estimate_ln_pi
 
+def load_in_window_gwas_betas_and_ses(window_gwas_summary_file, window_rsids):
+	f = open(window_gwas_summary_file)
+	beta_vec =[]
+	beta_se_vec = []
+	rsid_vec = []
+	head_count = 0
+	for line in f:
+		line = line.rstrip()
+		data = line.split('\t')
+		if head_count == 0:
+			head_count = head_count + 1
+			continue
+		rsid = data[0]
+		beta = float(data[1])
+		beta_se = float(data[2])
+		beta_vec.append(beta)
+		beta_se_vec.append(beta_se)
+		rsid_vec.append(rsid)
+
+	f.close()
+
+	if np.array_equal(np.asarray(rsid_vec), window_rsids) == False:
+		print('assumption eroror')
+		pdb.set_trace()
+
+	return np.asarray(beta_vec), np.asarray(beta_se_vec)
+
+# Convert gwas summary statistics to *STANDARDIZED* effect sizes
+# Following SuSiE code found in these two places:
+########1. https://github.com/stephenslab/susieR/blob/master/R/susie_rss.R  (LINES 277-279)
+########2. https://github.com/stephenslab/susieR/blob/master/R/susie_ss.R (LINES 148-156 AND 203-205)
+def convert_to_standardized_summary_statistics(gwas_beta_raw, gwas_beta_se_raw, gwas_sample_size, R, sigma2=1.0):
+	gwas_z_raw = gwas_beta_raw/gwas_beta_se_raw
+
+	XtX = (gwas_sample_size-1)*R
+	Xty = np.sqrt(gwas_sample_size-1)*gwas_z_raw
+	var_y = 1
+
+	dXtX = np.diag(XtX)
+	csd = np.sqrt(dXtX/(gwas_sample_size-1))
+	csd[csd == 0] = 1
+
+	XtX = (np.transpose((1/csd) * XtX) / csd)
+	Xty = Xty / csd
+
+	dXtX2 = np.diag(XtX)
+
+	beta_scaled = (1/dXtX2)*Xty
+	beta_se_scaled = np.sqrt(sigma2/dXtX2)
+
+	return beta_scaled, beta_se_scaled, XtX
+
+
+def save_ln_pi_output_file(ln_pi, output_file, window_rsids, gene_tissue_pairs):
+	t2 = open(output_file,'w')
+	t2.write('element_name\tln_pi\n')
+
+	genetic_element_names = np.hstack((window_rsids, gene_tissue_pairs))
+
+	# Quick error check
+	if len(genetic_element_names) != len(ln_pi):
+		print('assumption error')
+		pdb.set_trace()
+
+	for itera, genetic_element_name in enumerate(genetic_element_names):
+		t2.write(genetic_element_name + '\t' + str(ln_pi[itera]) + '\n')
+
+	t2.close()
+
+	return
 
 
 #####################
@@ -198,6 +268,15 @@ sparse_sldsc_tau = sparse_sldsc_tau_raw[2:,1].astype(float)
 rsid_to_genomic_annotation, variant_position_vec, rsids = create_dictionary_mapping_from_rsid_to_genomic_annotation_vector(annotation_file)
 
 
+# Open outputful summarizing TGFM input (one line for each window)
+tgfm_input_data_summary_file = simulated_tgfm_input_data_dir + simulation_name_string + '_tgfm_input_data_summary.txt'
+t = open(tgfm_input_data_summary_file,'w')
+# Write header
+t.write('window_name\tLD_npy_file\tTGFM_input_pkl\tlog_prior_probability_file_stem\n')
+
+
+
+
 # Now loop through windows
 f = open(simulation_window_list_file)
 head_count = 0
@@ -225,16 +304,52 @@ for line in f:
 
 	# Extract gene-tissue pairs and fitted models in this window
 	gene_summary_file = simulated_gene_expression_dir + simulation_name_string + '_causal_eqtl_effect_summary.txt'
-	gene_tissue_pairs, gene_tissue_pair_weight_files, gene_tissue_pairs_tss = extract_gene_tissue_pairs_and_associated_gene_models_in_window(window_start, window_end, gene_summary_file, simulated_learned_gene_models_dir, simulation_name_string, eqtl_sample_size, window_indices)
+	gene_tissue_pairs, gene_tissue_pair_weight_vectors, gene_tissue_pairs_tss = extract_gene_tissue_pairs_and_associated_gene_models_in_window(window_start, window_end, gene_summary_file, simulated_learned_gene_models_dir, simulation_name_string, eqtl_sample_size, window_indices)
 
 	# Extract LD
 	ld_mat_file = simulated_tgfm_input_data_dir + simulation_name_string + '_' + window_name + '_in_sample_ld.npy'
 	ld_mat = np.load(ld_mat_file)
 
-	# Compute various ln(pi)
-	point_estimate_ln_pi, sparse_estimate_ln_pi, distribution_estimate_ln_pi = compute_various_versions_of_log_prior_probabilities(window_rsids, window_anno_mat, gene_tissue_pairs, sldsc_tau_mean, sldsc_tau_cov, sparse_sldsc_tau)
+	# Get middle variant indices and middle gene indices
+	middle_variant_indices = np.where((variant_position_vec >= window_middle_start) & (variant_position_vec < window_middle_end))[0]
+	middle_gene_indices = np.where((gene_tissue_pairs_tss >= window_middle_start) & (gene_tissue_pairs_tss < window_middle_end))[0]
+
+	# Load in GWAS betas and standard errors
+	window_gwas_summary_file = simulated_gwas_dir + simulation_name_string + '_simualated_gwas_results_window_' + window_name + '.txt'
+	gwas_beta, gwas_beta_se = load_in_window_gwas_betas_and_ses(window_gwas_summary_file, window_rsids)
+	# Standardize gwas beta and se
+	beta_scaled, beta_se_scaled, XtX = convert_to_standardized_summary_statistics(gwas_beta, gwas_beta_se, n_gwas_individuals, ld_mat)
+
+	# Compute various ln(pi) and save to output # and save those results to output files
+	ln_pi_output_stem = simulated_tgfm_input_data_dir + simulation_name_string + '_' + window_name + '_ln_pi'
+	thresholds = [1e-10, 1e-11, 1e-12, 1e-13, 1e-14, 1e-15, 1e-20, 1e-30]
+	for threshold in thresholds:
+		point_estimate_ln_pi, sparse_estimate_ln_pi, distribution_estimate_ln_pi = compute_various_versions_of_log_prior_probabilities(window_rsids, window_anno_mat, gene_tissue_pairs, sldsc_tau_mean, sldsc_tau_cov, sparse_sldsc_tau, threshold=threshold)
+		save_ln_pi_output_file(point_estimate_ln_pi, ln_pi_output_stem + '_point_estimate_' + str(threshold) + '.txt', window_rsids, gene_tissue_pairs)
+		save_ln_pi_output_file(sparse_estimate_ln_pi, ln_pi_output_stem + '_sparse_estimate_' + str(threshold) + '.txt', window_rsids, gene_tissue_pairs)
+		save_ln_pi_output_file(distribution_estimate_ln_pi, ln_pi_output_stem + '_distribution_estimate_' + str(threshold) + '.txt', window_rsids, gene_tissue_pairs)
 
 
+	# Organize TGFM data into nice data structure
+	tgfm_data = {}
+	tgfm_data['genes'] = gene_tissue_pairs
+	tgfm_data['variants'] = window_rsids
+	tgfm_data['gwas_beta'] = beta_scaled
+	tgfm_data['gwas_beta_se'] = beta_se_scaled
+	tgfm_data['gwas_sample_size'] = n_gwas_individuals
+	tgfm_data['gene_eqtl_pmces'] = gene_tissue_pair_weight_vectors
+
+	# Save TGFM output data to pickle
+	window_pickle_output_file = simulated_tgfm_input_data_dir + simulation_name_string + '_' + window_name + '_tgfm_input_data.pkl'
+	g = open(window_pickle_output_file, "wb")
+	pickle.dump(tgfm_data, g)
+	g.close()
+
+
+	# Write to output summary file
+	t.write(window_name + '\t' + ld_mat_file + '\t' + window_pickle_output_file + '\t' + ln_pi_output_stem + '\n')
+
+t.close()
 f.close()
 
 
